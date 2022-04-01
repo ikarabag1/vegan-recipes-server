f95df262bb64816ee6e680a7b578d7fe108ba56a
“ikarabag1” <irmakkarabag@gmail.com>
2022-04-01 07:54:07 +0300

    delete route to destroy specific recipe is added and functional

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -19,17 +19,17 @@ router.get('/', async (req, res) =>{
 })
 
 // // | GET | index | all recipes created by a specific user | --working good with the one on top but in conflict with single recipe GET show route???
-// router.get('/:userid', async (req, res) => {
-//     try {
-//         const allRecipes = await db.Recipe.find({user:req.params.userid})
-//         res.json(allRecipes)
-//     } catch (err) {
-//         console.log(err)
-//         res.status(503).json({
-//             message: 'the db server is down'
-//         })
-//     }
-// })
+router.get('/:userid', async (req, res) => {
+    try {
+        const allRecipes = await db.Recipe.find({user:req.params.userid})
+        res.json(allRecipes)
+    } catch (err) {
+        console.log(err)
+        res.status(503).json({
+            message: 'the db server is down'
+        })
+    }
+})
 
 // | POST | create | add a new recipe by a specific user | *** working fine
 router.post('/:userid', async (req, res) => {
@@ -54,24 +54,24 @@ router.post('/:userid', async (req, res) => {
 })
 
 // // | GET | show | show the specific recipe of the user | --working fine with the first one but in conflict with all reciepes bu a specific user GET route
-router.get('/:id', async (req, res) => {
-    try {
-        const recipe = await db.Recipe.findById(req.params.id)
-            if (!recipe)
-                return res.status(404).json({
-                    message: 'recipe can not found'
-                })
-            res.json(recipe)
-    } catch (err) {
-            if (err.name === 'CastError')
-                return res.status(404).json({
-                    message: 'recipe not found, id error'
-                })
-            res.status(503).json({
-                message: 'server burned down'
-            })
-        }
-})
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const recipe = await db.Recipe.findById(req.params.id)
+//             if (!recipe)
+//                 return res.status(404).json({
+//                     message: 'recipe can not found'
+//                 })
+//             res.json(recipe)
+//     } catch (err) {
+//             if (err.name === 'CastError')
+//                 return res.status(404).json({
+//                     message: 'recipe not found, id error'
+//                 })
+//             res.status(503).json({
+//                 message: 'server burned down'
+//             })
+//         }
+// })
 
 // | PUT/PATCH | update | edit a recipe | *** working fine
 router.put('/:id', async (req, res) => {
@@ -88,6 +88,23 @@ router.put('/:id', async (req, res) => {
             message: 'server won\'t do that'
         })
     }
+})
+
+// | DELETE | destroy sprecific recipe | *** 
+router.delete('/:id', async (req, res) => {
+    try {
+        const recipe = await db.Recipe.findByIdAndDelete(req.params.id)
+        if (!recipe)
+        return res.status(404).json({
+            message: 'not atm'
+        })
+    res.json(recipe)
+} catch (err) {
+    // console.log(err)
+    res.status(503).json({
+        message: 'server won\'t do that'
+    })
+}
 })
 
 // module.export
